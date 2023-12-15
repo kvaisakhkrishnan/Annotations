17892fc4e9d6c9439fd54ce3bfffbd5891c4c7ea
Vaisakhkrishnan K <cb.en.u4cse20069@cb.students.amrita.edu>
2023-12-15 08:29:31 +0530

    Added RealAll

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,12 +1,14 @@
 import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 import com.opencsv.exceptions.CsvValidationException;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
 
 public class Driver {
-    public static void main(String[] args) {
+    public static void readOneByOne(){
         String fileName = "/Users/vaisakhkrishnank/IdeaProjects/Annotations/src/input.csv";
         try{
             CSVReader reader = new CSVReader(new FileReader(fileName));
@@ -46,5 +48,37 @@ public class Driver {
         catch (IOException | CsvValidationException err) {
             err.printStackTrace();
         }
+    }
+    public static void readAllTogether(){
+        String fileName = "/Users/vaisakhkrishnank/IdeaProjects/Annotations/src/input.csv";
+        try{
+            CSVReader reader = new CSVReader(new FileReader(fileName));
+            List<String[]> data;
+            data = reader.readAll();
+            for(String[] it : data){
+                System.out.print(it[0]);
+                System.out.print("\t");
+                System.out.print("|");
+                System.out.print("\t");
+                System.out.print(it[1]);
+                System.out.print("\t");
+                System.out.print("|");
+                System.out.print("\t");
+                System.out.print(it[2]);
+                System.out.print("\t");
+                System.out.print("|");
+                System.out.print("\t");
+                System.out.println(it[3]);
+                System.out.println("==============================================");
+
+            }
+        }
+        catch (IOException | CsvException err) {
+            err.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        readOneByOne();
+        readAllTogether();
     }
 }
